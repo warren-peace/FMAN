41c8928f1f19b7e831437a6a5533a2dc71f90b29
warren-peace <screw.weee@gmail.com>
2016-12-04 05:58:50 +0800

    sql update with fake sample

--- sql structures/fin-man.sql
+++ sql structures/fin-man.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 02, 2016 at 04:12 AM
+-- Generation Time: Dec 03, 2016 at 10:57 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -28,12 +28,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `journal-entries` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` varchar(8) NOT NULL,
+  `date` varchar(10) NOT NULL,
   `account` varchar(50) NOT NULL,
   `type` varchar(6) NOT NULL,
   `amount` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Dumping data for table `journal-entries`
+--
+
+INSERT INTO `journal-entries` (`id`, `date`, `account`, `type`, `amount`) VALUES
+(5, '06/21/2016', 'cash', 'credit', '15000'),
+(6, '06/23/2016', 'cash', 'debit', '1000'),
+(7, '06/24/2016', 'equipment', 'debit', '20000'),
+(8, '06/24/2016', 'Accounts Payable', 'credit', '8000'),
+(9, '06/24/2016', 'Accounts Payable', 'debit', '25000'),
+(10, '06/24/2016', 'equipment', 'credit', '5000'),
+(14, '06/24/2016', 'cash', 'debit', '1200'),
+(15, '06/24/2016', 'cash', 'debit', '500'),
+(16, '06/24/2016', 'electricity', 'credit', '300');
 
 -- --------------------------------------------------------
 
@@ -43,10 +58,20 @@ CREATE TABLE IF NOT EXISTS `journal-entries` (
 
 CREATE TABLE IF NOT EXISTS `journal-transactions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` varchar(8) NOT NULL,
+  `date` varchar(10) NOT NULL,
   `name` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `journal-transactions`
+--
+
+INSERT INTO `journal-transactions` (`id`, `date`, `name`) VALUES
+(4, 'asfefef', 'wefewfwe argnar gajehrgk rgajkrhg akjgh argkjahr g'),
+(10, 'efw4g', 'g5t4h6'),
+(11, '23f5g5', '23ffrg'),
+(12, 'eeqf', 'afaef');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
