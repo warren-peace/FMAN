b9f7230866457e97673a5a3e5e4ef77c27ed3cdc
warren-peace <screw.weee@gmail.com>
2016-12-04 05:31:15 +0800

    credit binding

---
+++ getCredits.php
@@ -0,0 +1,18 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+  
+  require 'db_account.php';
+  $data = json_decode(file_get_contents("php://input"));
+  $aToFind = mysql_real_escape_string($data->aToFind);
+  $result = $conn->query("SELECT `amount` FROM `journal-entries` WHERE `account`='".$aToFind."' AND `type`='credit'");
+
+  $outp = '"';
+  while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+      $outp .= $rs["amount"] . '<br>';
+  }
+  $outp ='{"records":'.$outp.'"}';
+  $conn->close();
+
+  echo($outp);
+?>
